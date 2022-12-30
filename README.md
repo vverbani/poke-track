f30e2540c88efde2879cb505a045ade7abe62217
Valmir Verbani <valverbani@gmail.com>
2022-12-30 18:58:15 +0100

    Retrieve average card price per set

--- README.md
+++ README.md
@@ -7,12 +7,12 @@ Poke Track is a Python script that utilizes the Pokemon TCG SDK to help track wh
 ## Getting Started
 
 1. Install [Docker](www.docker.com)
-2. Get original [spreadsheet access](https://docs.google.com/spreadsheets/d/1rnqsBE5UIo6HFaGAQSrVzzk18lFTGLS17IVE4oIB4qw/edit?usp=sharing). Then download the new spreadsheet locally
+2. Request access to spreadsheet
 3. Change input and output file paths and file path names then copy run `cp .env.example .env`
 4. Run ``` docker-compose up ```
 5. Import `poke-spread-w-card_prices.csv` on your spreadsheet as a `new sheet`
 6. Copy all data from new sheet onto your main sheet. Right click, paste special, paste only values.
 
-<img src="https://github.com/vverbani/poke-track/blob/main/src/spreadsheet/spreadsheet-screenshot.png" alt="Spreadsheet of Pokemon Card purchases versus online value" title="Spreadsheet of Pokemon Card purchases versus online value">
+<img src="https://github.com/vverbani/poke-track/blob/main/src/spreadsheet/spreadsheet-screenshot1.png" alt="Spreadsheet of Pokemon Card purchases versus online value" title="Spreadsheet of Pokemon Card purchases versus online value">
 
 *** Cards and prices shown in spreadsheet are randomized ***
--- src/main.py
+++ src/main.py
@@ -38,41 +38,59 @@ def add_list_to_csv(list):
         for row in list:
             writer.writerow(row)
 
-def card_price_per_set(set):
+def card_price_per_set(list, currency):
     # List Example: [{set, average_price, over_10, over_20, over_50, over_100}]
+    card_prices_per_set=[]
+    avg_price, over_10, over_20, over_50, over_100= 0,0,0,0,0
     card_query= ''
-    average_price= 0
 
-    # If set contains multiple words, i.e Silver Tempest, Silver Tempest Trainer Gallery add quotes around it other wise call fails
-    if len(set.split()) > 1:
-        card_query='set.name:' + '"' + set + '"' + ' supertype:pokemon'
-    else:
-        card_query='set.name:' + set + ' supertype:pokemon'
+    for idx, set in enumerate(list):
+        # If set contains multiple words, i.e Silver Tempest, Silver Tempest Trainer Gallery add quotes around it other wise call fails
+        if len(set.split()) > 1:
+            card_query='set.name:' + '"' + set + '"' + ' supertype:pokemon'
+        else:
+            card_query='set.name:' + set + ' supertype:pokemon'
 
-    cards= Card.where(q=card_query)
+        cards= Card.where(q=card_query)
 
-    for card in cards:
-        # API has some cards at 'None' - potentially because of no sale/list data?
-        if card.cardmarket.prices and card.cardmarket.prices.trendPrice:
-            average_price += card.cardmarket.prices.trendPrice
+        for card in cards:
+            card_temp_price=0
+            # API has some cards at 'None' - potentially because of no sale/list data?
+            if card.cardmarket.prices and card.cardmarket.prices.trendPrice:
+                # Convert each price to USD and then round it to 2 decimals
+                card_temp_price= currency.convert(float(card.cardmarket.prices.trendPrice), 'EUR', 'USD')
+                card_temp_price= round(card_temp_price, 2)
 
-    # Needs to change to right currency and round to the nearest cents
-    average_price= average_price / len(cards)
+                avg_price += card_temp_price
 
-    return average_price
+                # Get number of cards over $10, $20, $50, $100
+                if card_temp_price > 10: over_10 += 1
+                if card_temp_price > 20: over_20 += 1
+                if card_temp_price > 50: over_50 += 1
+                if card_temp_price > 100: over_100 += 1
+
+        # Needs to change to right currency and round to the nearest cents
+        avg_price= avg_price / len(cards)
+
+        # Add values to list
+        card_prices_per_set.append([{"set": set}, {"avg_price": avg_price}, {"over_10": over_10}, {"over_20": over_20}, {"over_50": over_50}, {"over_100": over_100}])
+
+    return card_prices_per_set
 
 def main():
     print("Starting ... .. . . . . . ...")
 
     temp_set_id= ''
     poke_list= []
-    set_price_list= []
+    # List of all the sets in the spreadsheet
+    set_list= []
 
     # Prepare currency converter
     currency= CurrencyConverter()
 
     poke_purchases= csv_to_list()
 
+    # Adding card prices only
     for poke in poke_purchases:
         # Only get prices if a Card has a given Description, Set, and a Card number
         if poke[0] != '' and poke[1] == 'Card' and poke[2] != 'n/a' and poke[4] != '':
@@ -88,6 +106,9 @@ def main():
 
             card= Card.find(temp_set_id)
 
+            # Add only unique set names to list
+            if poke[4] not in set_list: set_list.append(poke[4])
+
             # Convert the price of the card from EUROS (default) into USD then add to row under Online Value (TCG)
             card_price= currency.convert(float(card.cardmarket.prices.trendPrice), 'EUR', 'USD')
             poke[11]= round(card_price,2)
@@ -100,6 +121,13 @@ def main():
 
         # Add each new row to the new list before writing it to the CSV file
         poke_list.append(poke)
+
+    # Getting price list of card per set and then adding to list before adding it to csv
+    set_list= card_price_per_set(set_list, currency)
+    for poke in poke_purchases:
+        for price in set_list:
+            if poke[4] == price[0]['set']:
+                poke[13]= round(price[1]['avg_price'], 2)
 
     add_list_to_csv(poke_list)
 
